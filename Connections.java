ef113857207be0810d9ee57e001e1f7dd2a43955
ethanwwm <33324347+ethanwwm@users.noreply.github.com>
2018-12-28 16:22:12 -0500

    Add files via upload

---
+++ Connections.java
@@ -0,0 +1,55 @@
+/*
+ * Author: Ethan Wong
+ * 
+ * Question: 1.5.19 from Robert Sedgewick and Kevin Wayne:
+ * Introduction to Programming in Java: An Interdisciplinary Approach
+ * 
+ * Takes as command-line arguments an integer n and
+ * a floating-point number p (between 0 and 1), plots n equally 
+ * spaced points on the circumference of a circle, and then, with 
+ * probability p for each pair of points, draws a gray line connecting them.
+ */
+public class Connections {
+
+	public static void main(String[]args) {
+		
+		if (args.length==0)
+			args = new String[] {"16", "1.0"};
+		
+		int n = Integer.parseInt(args[0]);
+		double p = Double.parseDouble(args[1]);
+		double a = 0;
+		double b = 0;
+		
+		//Sets the scale of the canvas
+		StdDraw.setXscale(-1.2, 1.2);
+		StdDraw.setYscale(-1.2, 1.2);
+		
+		//Draws the set number of points on the circumference of the circle
+		for(int i = 0; i < n; i++) {
+			StdDraw.setPenRadius(0.05);
+			StdDraw.point(Math.cos(a), Math.sin(a));
+			a = a + 2 * Math.PI / n;
+		}
+		
+		//Nested loop
+		for(int j = 0; j < n; j++) {
+			a = 0;
+			
+			for(int k = 0; k < n; k++) {
+				//Draws a random number and assigns it to RanNum
+				double RanNum = (double) Math.random();
+				
+				StdDraw.setPenRadius(0.005);
+				
+				//Compares tha random number to the probability of it being drawn
+				if (RanNum <= p)
+					StdDraw.line(Math.cos(b), Math.sin(b), Math.cos(a), Math.sin(a));
+				
+				a = a + 2 * Math.PI / n;
+			}
+			
+			b = b + 2 * Math.PI / n;
+		}
+	}
+}
